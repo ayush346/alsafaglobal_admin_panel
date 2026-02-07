49d9ed3f79d2094d8523a50a8f73b5dd94899188
Ayush BC <ayushbargal2002@gmail.com>
2026-02-07 17:12:51 +0530

    ghsgdfdsf

--- cms/schemaTypes/productsPage.js
+++ cms/schemaTypes/productsPage.js
@@ -4,44 +4,6 @@ export default {
     type: 'document',
   
     fields: [
-      // PRODUCT GROUPS
-      {
-        name: 'productGroups',
-        title: 'Product Groups',
-        type: 'array',
-        of: [
-          {
-            type: 'object',
-            fields: [
-              {
-                name: 'title',
-                title: 'Group Title',
-                type: 'string',
-              },
-              {
-                name: 'products',
-                title: 'Products in Group',
-                type: 'array',
-                of: [
-                  {
-                    type: 'string',
-                    title: 'Product Slug',
-                    description: 'Enter the slug of a product from the products list above.'
-                  }
-                ],
-                description: 'Select products to include in this group.'
-              },
-              {
-                name: 'segmentRef',
-                title: 'Link to Segment',
-                type: 'reference',
-                to: [{ type: 'segment' }],
-                description: 'The "View Services" button for this group will link to this segment.'
-              }
-            ]
-          }
-        ]
-      },
   
       // PAGE HEADER
       {
