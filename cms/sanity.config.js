2db73e12315883f0f7be69d18a792e6e124b3d83
Ayush BC <ayushbargal2002@gmail.com>
2026-02-08 17:29:38 +0530

    okay

--- cms/sanity.config.js
+++ cms/sanity.config.js
@@ -10,31 +10,7 @@ export default defineConfig({
   projectId: '5d3fj7tm',
   dataset: 'production',
 
-  plugins: [
-    structureTool({
-      structure: (S) =>
-        S.list()
-          .title('Content')
-          .items([
-            S.listItem()
-              .title('Home Page')
-              .child(S.document().schemaType('homePage').documentId('homePage')),
-            S.listItem()
-              .title('About Us Page')
-              .child(S.document().schemaType('aboutPage').documentId('aboutPage')),
-            S.listItem()
-              .title('Segments Page')
-              .child(S.document().schemaType('segmentsPage').documentId('segmentsPage')),
-            S.listItem()
-              .title('Products Page')
-              .child(S.document().schemaType('productsPage').documentId('productsPage')),
-            S.listItem()
-              .title('Contact Page')
-              .child(S.document().schemaType('contactPage').documentId('contactPage')),
-          ]),
-    }),
-    visionTool(),
-  ],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
--- cms/schemaTypes/index.js
+++ cms/schemaTypes/index.js
@@ -1,15 +1,17 @@
 import homePage from './homePage'
 import aboutPage from './aboutPage'
 import segmentsPage from './segmentsPage'
-import productsPage from './productsPage'
 import contactPage from './contactPage'
+import productsPage from './productsPage'
+
+
 
 export const schemaTypes = [
   homePage,
   aboutPage,
   segmentsPage,
-  productsPage,
   contactPage,
+  productsPage,
 ]
 
   
